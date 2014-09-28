568a5faf03ad14e7e0e1410e16cf58b684f9d3ed
Floats <wqzhiep@gmail.com>
2014-09-28 18:27:23 +0800

    clean up the code of hash_table

--- mstd/test/hash_table.cpp
+++ mstd/test/hash_table.cpp
@@ -18,6 +18,51 @@ BOOST_AUTO_TEST_CASE(hash_table_iterators)
     BOOST_REQUIRE(table.cbegin() == table.cend());
     BOOST_REQUIRE(ctable.begin() == ctable.end());
     BOOST_REQUIRE(ctable.cbegin() == ctable.cend());
+
+    BOOST_REQUIRE(table.begin() == table.cbegin());
+    BOOST_REQUIRE(table.end() == table.cend());
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_iterators_ops)
+{
+    mstd::detail::hash_table<int, int> table;
+
+    table.insert_unique(1);
+    BOOST_REQUIRE(table.begin() != table.end());
+    BOOST_REQUIRE(table.cbegin() != table.cend());
+
+    auto i = table.begin();
+    ++i;
+    BOOST_REQUIRE(i == table.end());
+
+    table.insert_unique(2);
+    table.insert_dup(1);
+
+    auto iter = table.find(1);
+
+    BOOST_REQUIRE_EQUAL(*iter, 1);
+    ++iter;
+    BOOST_REQUIRE_EQUAL(*iter, 1);
+    ++iter;
+    if (iter != table.end()) {
+        BOOST_REQUIRE_EQUAL(*iter, 2);
+    }
+
+    table.insert_dup(2);
+    table.insert_dup(2);
+
+    auto cnt = table.count(2);
+    BOOST_REQUIRE_EQUAL(cnt, 3);
+
+    auto iters = table.equal_range(2);
+    auto first = iters.first;
+    BOOST_REQUIRE(first != table.end());
+    BOOST_REQUIRE(first != iters.second);
+    for (; cnt > 0; ++first, --cnt) {
+        BOOST_REQUIRE_EQUAL(*first, 2);
+    }
+    BOOST_REQUIRE_EQUAL(mstd::distance(iters.first, iters.second), 3);
+    BOOST_REQUIRE(first == iters.second);
 }
 
 BOOST_AUTO_TEST_CASE(hash_table_insert_1)
@@ -74,7 +119,7 @@ BOOST_AUTO_TEST_CASE(hash_table_insert)
     BOOST_REQUIRE(!table.empty());
 }
 
-BOOST_AUTO_TEST_CASE(hash_table_lookup)
+BOOST_AUTO_TEST_CASE(hash_table_lookup_find)
 {
     mstd::detail::hash_table<int, int> table;
 
@@ -90,17 +135,32 @@ BOOST_AUTO_TEST_CASE(hash_table_lookup)
         BOOST_REQUIRE(iter != table.end());
     }
 
-
     for (auto x : { 0, 5, 7}) {
         BOOST_REQUIRE(table.find(x) == table.end());
+    }
+}
+
+BOOST_AUTO_TEST_CASE(hash_table_lookup_equal_range)
+{
+    mstd::detail::hash_table<int, int> table;
+
+    auto list = {1, 3, 4, 2, 9};
+
+    for (auto x : list) {
+        table.insert_unique(x);
     }
 
     for (auto x : list) {
         auto iters = table.equal_range(x);
 
         BOOST_REQUIRE_EQUAL(table.count(x), 1);
+
+        std::for_each(iters.first, iters.second, [x](int val){
+            BOOST_REQUIRE_EQUAL(val, x);
+        });
+
         BOOST_REQUIRE(iters.first != table.end());
-        BOOST_REQUIRE(mstd::distance(iters.first, iters.second) == 1);
+        BOOST_REQUIRE_EQUAL(mstd::distance(iters.first, iters.second), 1);
     }
 
     for (auto x : { 0, 5, 7}) {
