ac90933aa06c80db254d7b6d7ca5c35dd73cd6cf
Floats <wqzhiep@gmail.com>
2014-10-02 21:52:01 +0800

    algorithm::reverse

--- mstd/algorithm/reverse.hpp
+++ mstd/algorithm/reverse.hpp
@@ -2,9 +2,28 @@
 #define MSTD_ALGORITHM_REVERSE_HPP_
 
 #include <mstd/iterator/concept.hpp>
+#include <mstd/algorithm/swap.hpp>
 
 namespace mstd {
+    template <class Iter>
+    Require<BidirectionalIterator<Iter>() && !RandomAccessIterator<Iter>()>
+        reverse(Iter first, Iter last)
+    {
+        for (; first != last; ++first) {
+            if (first != --last) {
+                mstd::iter_swap(first, last);
+            }
+        }
+    }
 
+    template <class Iter>
+    Require<RandomAccessIterator<Iter>()>
+        reverse(Iter first, Iter last)
+    {
+        for (; first < last; ) {
+            mstd::iter_swap(first++, --last);
+        }
+    }
 }  // of namespace mstd
 
 #endif //! MSTD_ALGORITHM_REVERSE_HPP_
